deab91f473b52c1f0a9348f89cc07d87b3546cd0
Kirill Romanov <djaler1@gmail.com>
2020-07-16 21:39:03 +0300

    qwe

--- jest.config.js
+++ jest.config.js
@@ -4,9 +4,6 @@ const { compilerOptions } = require('./tsconfig');
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    collectCoverageFrom: ['src/**'],
-    coverageDirectory: 'reports/coverage',
-    coverageReporters: ['lcov'],
     moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
     testMatch: [
         '<rootDir>/tests/**/*.spec.ts',
